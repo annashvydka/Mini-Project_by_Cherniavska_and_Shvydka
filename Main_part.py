00981ced231c866a42c0f482e385873a63cacbab
olhac <olha.cherniavska.25@kse.org.ua>
2026-07-23 23:21:10 +0300

    Розрахований прибуток/збиток (P&L) на основі сигналів

--- Main_part.py
+++ Main_part.py
@@ -3,11 +3,15 @@
 
 import yfinance as yf
 import pandas as pd
+
 #2
 data = yf.download("SBUX", start="2025-01-01", end="2026-01-01")
 
 df = data.reset_index()  #бо дата була рядками, а тепер стала окремою колонкою і замість дат 123
 df = df[["Date", "Open", "High", "Low", "Close", "Volume"]]  #з усіх даних візьмемо лише ці колонкі
+
+#print(df.columns)
+df.columns = df.columns.get_level_values(0)  #прибирає з назв колнок 'SBUX'
 
 #3
 short_period= 7
@@ -23,10 +27,30 @@ for row in range( long_period, len(df)):
     long_mov_aver= df.loc[row, "Long_Moving_Average"]
 
     if short_mov_aver > long_mov_aver:
-        df.loc[row, "Signal"] = 1
+        df.loc[row, "Signal"] = 1 #купівля
     elif short_mov_aver < long_mov_aver:
-        df.loc[row, "Signal"] = -1
+        df.loc[row, "Signal"] = -1  #продаж
     else:
-        df.loc[row, "Signal"] = 0
+        df.loc[row, "Signal"] = 0  #утримання
+
+
+#4.1
+df["Price_Change"] = 0.0
+
+for row in range(1, len(df)):
+    df.loc[row, "Price_Change"] = df.loc[row, "Close"] - df.loc[row - 1, "Close"]
+
+
+df["Profit"] = 0.0
+for row in range(1, len(df)):
+    if df.loc[row, "Signal"] == 1:
+        df.loc[row, "Profit"] = df.loc[row, "Price_Change"]
+    elif df.loc[row, "Signal"] == -1:
+        df.loc[row, "Profit"] = -df.loc[row, "Price_Change"] #якщо ціна впала ("Price_Change"<0) то ми наче заробили невтрачену суму
+    else:
+        df.loc[row, "Profit"] = 0
+
 print(df)
 
+total_profit = df["Profit"].sum()
+print(total_profit)
