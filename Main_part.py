e60b8149ef9c644e6d608f72207ab41f1d925dcf
Anna Shvydka <anna.shvydka.25@kse.org.ua>
2026-07-23 22:03:39 +0300

    Реалізовано розрахунок короткострокових і довгострокових ковзних середніх. Прописана логіка генерації торгових сигналів на історичних даних.

--- Main_part.py
+++ Main_part.py
@@ -3,10 +3,29 @@
 
 import yfinance as yf
 import pandas as pd
-
+#2
 data = yf.download("SBUX", start="2025-01-01", end="2026-01-01")
 
 df = data.reset_index()  #бо дата була рядками, а тепер стала окремою колонкою і замість дат 123
 df = df[["Date", "Open", "High", "Low", "Close", "Volume"]]  #з усіх даних візьмемо лише ці колонкі
 
-print(df.head())
+#3
+short_period= 7
+long_period = 30
+
+df["Short_Moving_Average"] = df["Close"].rolling(short_period).mean()
+df["Long_Moving_Average"] = df["Close"].rolling(long_period).mean()
+
+df["Signal"] = 0 #нова колонка сигналів
+
+for row in range( long_period, len(df)):
+    short_mov_aver= df["Short_Moving_Average"].iloc[row]
+    long_mov_aver= df["Long_Moving_Average"].iloc[row]
+    if short_mov_aver > long_mov_aver:
+        df.loc[df.index[row], "Signal"] = 1
+    elif short_mov_aver < long_mov_aver:
+        df.loc[df.index[row], "Signal"] = -1
+    else:
+        df.loc[df.index[row], "Signal"] = 0
+print(df)
+
